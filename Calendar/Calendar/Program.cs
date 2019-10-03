531142e2d0348cdfaf5fcd11bbe29ee246522e01
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-10-03 16:56:01 +0200

    tried to add apointment (link = https://support.microsoft.com/en-us/help/310259/how-to-use-the-microsoft-outlook-object-library-to-retrieve-an-appoint)
    not yet - soon :))

--- Calendar/Calendar/Program.cs
+++ Calendar/Calendar/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
 
+using System.Reflection;     // to use Missing.Value
+
+//TO DO: If you use the Microsoft Outlook 11.0 Object Library, uncomment the following line.
+using Outlook = Microsoft.Office.Interop.Outlook;
+
 namespace Calendar
 {
     class Program
@@ -10,6 +15,28 @@ namespace Calendar
 
 
 
+            Outlook.Application oApp = new Outlook.Application();
+
+            // Get the NameSpace and Logon information.
+            // Outlook.NameSpace oNS = (Outlook.NameSpace)oApp.GetNamespace("mapi");
+            Outlook.NameSpace oNS = oApp.GetNamespace("mapi");
+
+            //Log on by using a dialog box to choose the profile.
+            oNS.Logon(Missing.Value, Missing.Value, true, true);
+
+            //Alternate logon method that uses a specific profile.
+            // TODO: If you use this logon method, 
+            // change the profile name to an appropriate value.
+            //oNS.Logon("YourValidProfile", Missing.Value, false, true); 
+
+            // Get the Calendar folder.
+            Outlook.MAPIFolder oCalendar = oNS.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar);
+
+            // Get the Items (Appointments) collection from the Calendar folder.
+            Outlook.Items oItems = oCalendar.Items;
+
+            // Get the first item.
+            Outlook.AppointmentItem oAppt = (Outlook.AppointmentItem)oItems.GetFirst();
 
         }
     }
