388601725e9a5270457eca0e6bab3dd36fe9a936
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-10-03 16:46:04 +0200

    noting

---
+++ Calendar/Calendar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calendar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! How about to try create an outlook event :-)");
+
+
+
+
+        }
+    }
+}
